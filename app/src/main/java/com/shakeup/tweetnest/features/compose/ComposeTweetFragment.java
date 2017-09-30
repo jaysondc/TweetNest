f822d20a083c46bcb7ecbbf3e8fcd8fdbcef4bbc
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-09-29 21:39:17 -0700

    Added layout for composing a tweet.

---
+++ app/src/main/java/com/shakeup/tweetnest/features/compose/ComposeTweetFragment.java
@@ -0,0 +1,17 @@
+package com.shakeup.tweetnest.features.compose;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Jayson on 9/29/2017.
+ *
+ * Fragment for composing new tweets
+ */
+
+public class ComposeTweetFragment extends DialogFragment {
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//
+//    }
+}
