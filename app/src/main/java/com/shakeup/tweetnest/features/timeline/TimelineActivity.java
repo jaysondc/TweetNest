76bc47666ba95939782e525059b7be8ed65b1ea6
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-10-05 15:44:38 -0700

    Added button to view current user profile.

--- app/src/main/java/com/shakeup/tweetnest/features/timeline/TimelineActivity.java
+++ app/src/main/java/com/shakeup/tweetnest/features/timeline/TimelineActivity.java
@@ -1,13 +1,19 @@
 package com.shakeup.tweetnest.features.timeline;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.shakeup.tweetnest.R;
+import com.shakeup.tweetnest.features.profile.ProfileActivity;
 import com.shakeup.tweetnest.features.timeline.adapters.TimelinePagerAdapter;
+import com.shakeup.tweetnest.features.timeline.home.HomeTimelineViewModel;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +24,8 @@ public class TimelineActivity extends AppCompatActivity {
 
     private final String TAG = this.getClass().getSimpleName();
 
+    private HomeTimelineViewModel mHomeTimelineViewModel;
+
     @BindView(R.id.toolbar_timeline)
     public Toolbar mToolbar;
 
@@ -27,12 +35,32 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
         ButterKnife.bind(this);
 
+        mHomeTimelineViewModel = ViewModelProviders.of(this).get(HomeTimelineViewModel.class);
+
         initToolbar();
         initPager();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_profile:
+                openUserProfile();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private void initToolbar() {
-        mToolbar.setTitle("Twitter!");
+        mToolbar.setTitle("Tweet Nest");
         this.setSupportActionBar(mToolbar);
     }
 
@@ -45,5 +73,13 @@ public class TimelineActivity extends AppCompatActivity {
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = findViewById(R.id.tab_timeline);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void openUserProfile() {
+        mHomeTimelineViewModel.getCurrentUser().observe(this, user -> {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            intent.putExtra(ProfileActivity.USER, user);
+            this.startActivity(intent);
+        });
     }
 }
